f5e235fee7d6fbc94184d40a9abd398bad82f308
Diego Amorim <diegoamorim03152004@gmail.com>
2024-10-12 22:33:28 -0300

    temporary finish project

--- ClientApp/src/app/Models/User.ts
+++ ClientApp/src/app/Models/User.ts
@@ -1,8 +1,6 @@
 export class User {
   email: string;
-  emailRepeat: string;
   password: string;
-  passwordRepeat: string;
   address: string;
   complement: string;
   city: string;
--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ClientApp';
