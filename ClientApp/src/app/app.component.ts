84b352a6e7165e87f295e240c272f1df7a315f76
diegoNZ04 <diegoamorim03152004@gmail.com>
2024-12-29 22:59:21 -0300

    finish: home-page

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,8 +8,6 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,12 +1,10 @@
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home-component', pathMatch: 'full'},
   {path: 'home-component', component: HomeComponent},
-  {path: 'login-component', component: LoginComponent},
   {path: 'register-component', component: RegisterComponent}
 ];
 
